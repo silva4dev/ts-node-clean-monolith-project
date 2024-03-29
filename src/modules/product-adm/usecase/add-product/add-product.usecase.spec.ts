67e1dc6f01e712cc02826389abf7625447768a28
silva4dev <web4dev.lucas@outlook.com>
2024-03-29 15:40:43 -0300

    test: add product usecase

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,33 @@
+import { AddProductInputDto } from "./add-product.dto";
+import AddProductUseCase from "./add-product.usecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
+
+describe("Add Product usecase unit test", () => {
+  it("should add a product", async () => {
+    // Repository
+    const productRepository = MockRepository();
+    // Usecase
+    const usecase = new AddProductUseCase(productRepository);
+    // Input
+    const input: AddProductInputDto = {
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+    // Output
+    const output = await usecase.execute(input);
+    expect(productRepository.add).toHaveBeenCalled();
+    expect(output.id).toBeDefined();
+    expect(output.name).toBe(input.name);
+    expect(output.description).toBe(input.description);
+    expect(output.purchasePrice).toBe(input.purchasePrice);
+    expect(output.stock).toBe(input.stock);
+  });
+});
