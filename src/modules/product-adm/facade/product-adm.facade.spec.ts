ef9b7e2d102e8a4be972f65868ca1820ff3a27e2
silva4dev <web4dev.lucas@outlook.com>
2024-03-31 21:15:11 -0300

    test: ensure add product adm facade factory

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "../repository/product.model";
-import ProductRepository from "../repository/product.repository";
-import AddProductUseCase from "../usecase/add-product/add-product.usecase";
-import ProductAdmFacade from "./product-adm.facade";
+import ProductAdmFacadeFactory from "../factory/facade.factory";
 
 describe("ProductAdmFacade test", () => {
   let sequelize: Sequelize;
@@ -24,12 +22,8 @@ describe("ProductAdmFacade test", () => {
   });
 
   it("should create a product", async () => {
-    const productRepository = new ProductRepository();
-    const addProductUseCase = new AddProductUseCase(productRepository);
-    const productFacade = new ProductAdmFacade({
-      addUsecase: addProductUseCase,
-      stockUsecase: undefined,
-    });
+    const productFacade = ProductAdmFacadeFactory.create();
+
     const input = {
       id: "1",
       name: "Product 1",
