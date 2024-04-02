b0e98e7841a1f411df3b35b1e71884c9478d6d04
silva4dev <web4dev.lucas@outlook.com>
2024-04-02 19:44:01 -0300

    refactor: ensure remove import unnecessary

--- src/modules/client-adm/usecase/add-client/add-client.usecase.ts
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -1,4 +1,3 @@
-import Id from "../../../@shared/domain/value-object/id.value-object";
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import Client from "../../domain/client.entity";
 import ClientGateway from "../../gateway/client.gateway";
