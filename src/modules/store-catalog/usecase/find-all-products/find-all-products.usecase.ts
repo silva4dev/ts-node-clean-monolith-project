8ea1e859f75fbd4812384a0013b3d7959ce94377
silva4dev <web4dev.lucas@outlook.com>
2024-04-01 22:24:03 -0300

    feat: ensure add dto

--- src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -1,5 +1,6 @@
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import ProductGateway from "../../gateway/product.gateway";
+import { findAllProductsOutputDto } from "./find-all-products.dto";
 
 export default class FindAllProductsUsecase implements UseCaseInterface {
   private _productRepository: ProductGateway;
@@ -8,7 +9,7 @@ export default class FindAllProductsUsecase implements UseCaseInterface {
     this._productRepository = _productRepository;
   }
 
-  async execute(): Promise<any> {
+  async execute(): Promise<findAllProductsOutputDto> {
     const products = await this._productRepository.findAll();
 
     return {
