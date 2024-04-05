d3ea3e09aad62acb82ef3223ba0ffb0f8efe9a82
silva4dev <web4dev.lucas@outlook.com>
2024-04-04 21:26:46 -0300

    test: ensure add client adm facade

---
+++ src/modules/client-adm/facade/client-adm.facade.spec.ts
@@ -0,0 +1,50 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "../repository/client.model";
+import ClientRepository from "../repository/client.repository";
+import AddClientUseCase from "../usecase/add-client/add-client.usecase";
+import ClientAdmFacade from "./client-adm.facade";
+
+describe("ClientAdmFacade test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ClientModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a client", async () => {
+    const clientRepository = new ClientRepository();
+    const addUsecase = new AddClientUseCase(clientRepository);
+    const facade = new ClientAdmFacade({
+      addUsecase,
+      findUsecase: undefined,
+    });
+
+    const input = {
+      id: "1",
+      name: "Client 1",
+      email: "x@x.com",
+      address: "Address 1",
+    };
+
+    await facade.add(input);
+
+    const client = await ClientModel.findOne({ where: { id: "1" } });
+
+    expect(client).toBeDefined();
+    expect(client.name).toBe(input.name);
+    expect(client.email).toBe(input.email);
+    expect(client.address).toBe(input.address);
+  });
+});
