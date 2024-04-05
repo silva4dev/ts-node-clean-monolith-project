bd336e1fac6e1517c54371d72104930b3825f512
silva4dev <web4dev.lucas@outlook.com>
2024-04-04 21:26:35 -0300

    feat: ensure add client usecase

--- src/modules/client-adm/usecase/add-client/add-client.usecase.ts
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -1,3 +1,4 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import Client from "../../domain/client.entity";
 import ClientGateway from "../../gateway/client.gateway";
@@ -12,6 +13,7 @@ export default class AddClientUseCase implements UseCaseInterface {
 
   async execute(input: AddClientInputDto): Promise<AddClientOutputDto> {
     const props = {
+      id: new Id(input.id) || new Id(),
       name: input.name,
       email: input.email,
       address: input.address,
