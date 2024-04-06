cc28e0e93bc693684cb9ec8713b0b80c7c5704c5
silva4dev <web4dev.lucas@outlook.com>
2024-04-06 16:31:07 -0300

    test: ensure add decline a transaction

--- src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -6,11 +6,25 @@ const transaction = new Transaction({
   id: new Id("1"),
   amount: 100,
   orderId: "1",
+  status: "approved",
 });
 
 const MockRepository = () => {
   return {
     save: jest.fn().mockReturnValue(Promise.resolve(transaction)),
+  };
+};
+
+const transaction2 = new Transaction({
+  id: new Id("1"),
+  amount: 50,
+  orderId: "1",
+  status: "declined",
+});
+
+const MockRepositoryDeclined = () => {
+  return {
+    save: jest.fn().mockReturnValue(Promise.resolve(transaction2)),
   };
 };
 
@@ -30,5 +44,22 @@ describe("Process payment usecase unit test", () => {
     expect(output.orderId).toBe("1");
     expect(output.createdAt).toBe(transaction.createdAt);
     expect(output.updatedAt).toBe(transaction.updatedAt);
+  });
+
+  it("should decline a transaction", async () => {
+    const transactionRepository = MockRepositoryDeclined();
+    const usecase = new ProcessPaymentUseCase(transactionRepository);
+    const input = {
+      orderId: "1",
+      amount: 50,
+    };
+    const output = await usecase.execute(input);
+    expect(output.transactionId).toBe(transaction2.id.id);
+    expect(transactionRepository.save).toHaveBeenCalled();
+    expect(output.status).toBe("declined");
+    expect(output.amount).toBe(50);
+    expect(output.orderId).toBe("1");
+    expect(output.createdAt).toBe(transaction2.createdAt);
+    expect(output.updatedAt).toBe(transaction2.updatedAt);
   });
 });
