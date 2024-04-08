64acfa8631a53dfe89bf1723f0acf78de835c9ff
silva4dev <web4dev.lucas@outlook.com>
2024-04-08 17:16:37 -0300

    feat: add invoice facade

---
+++ src/modules/invoice/facade/invoice.facade.ts
@@ -0,0 +1,32 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import InvoiceFacadeInterface, {
+  FindInvoiceFacadeInputDto,
+  FindInvoiceFacadeOutputDto,
+  GenerateInvoiceFacadeInputDto,
+  GenerateInvoiceFacadeOutputDto,
+} from "./invoice.facade.interface";
+export interface UseCaseProps {
+  generateUseCase: UseCaseInterface;
+  findUseCase: UseCaseInterface;
+}
+
+export default class InvoiceFacade implements InvoiceFacadeInterface {
+  private _generateUseCase: UseCaseInterface;
+  private _findUseCase: UseCaseInterface;
+
+  constructor(props: UseCaseProps) {
+    this._generateUseCase = props.generateUseCase;
+    this._findUseCase = props.findUseCase;
+  }
+
+  async create(
+    input: GenerateInvoiceFacadeInputDto
+  ): Promise<GenerateInvoiceFacadeOutputDto> {
+    return await this._generateUseCase.execute(input);
+  }
+  async find(
+    input: FindInvoiceFacadeInputDto
+  ): Promise<FindInvoiceFacadeOutputDto> {
+    return await this._findUseCase.execute(input);
+  }
+}
